2e7ef71f842c8971f3f70e3c5b47fe9b0e034a76
buddzbuddy <abormokov@yahoo.com>
2017-03-13 14:53:00 +0600

    standard named

--- BackgroundService/IService.cs
+++ BackgroundService/IService.cs
@@ -10,7 +10,7 @@ namespace BackgroundService
 {
     // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
     [ServiceContract]
-    public interface IService1
+    public interface IService
     {
 
         [OperationContract]
--- BackgroundService/Service.svc.cs
+++ BackgroundService/Service.svc.cs
@@ -10,7 +10,7 @@ namespace BackgroundService
 {
     // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде, SVC-файле и файле конфигурации.
     // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы Service1.svc или Service1.svc.cs в обозревателе решений и начните отладку.
-    public class Service1 : IService1
+    public class Service1 : IService
     {
         public string GetData(int value)
         {
